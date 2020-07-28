7c7688c576062e0b2227d50d6459880362cded1c
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-28 16:04:31 -0500

    meh

---
+++ hello.py
@@ -0,0 +1,5 @@
+def yourName():
+    name = input("What is your name? ")
+    print("Hello, " + name + "!")
+
+yourName()
---
+++ hello2.py
@@ -0,0 +1,7 @@
+def loudNames():
+    name = input("WHAT IS YOUR NAME? ")
+    length = len(name)
+    print("HELLO, " + name + "!")
+    print("YOUR NAME HAS " + str(length) + " LETTERS IN IT!")
+
+loudNames()
