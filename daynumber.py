19d41e7ccc8557482f659b25f398cfcedeccc144
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-28 16:14:59 -0500

    Initial commit

---
+++ daynumber.py
@@ -0,0 +1,18 @@
+def dayNumbers():
+    day = int(input("What day is it? "))
+    if day == 0:
+        print("Sunday")
+    elif day == 1:
+        print("Monday")
+    elif day == 2:
+        print("Tuesday")
+    elif day == 3:
+        print("Wednesday")
+    elif day == 4:
+        print("Thursday")
+    elif day == 5:
+        print("Friday")
+    elif day == 6:
+        print("Saturday")
+    
+dayNumbers()
