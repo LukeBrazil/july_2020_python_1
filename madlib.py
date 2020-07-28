7b122be60c8f4f22c56cf7d9d2a4224b5af4d85d
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-28 15:53:14 -0500

    Initial commit

---
+++ madlib.py
@@ -0,0 +1,9 @@
+def madLib():
+    print("Please fill in the blanks below:")
+    print("___(name)___'s favorite subject in school is ___(subject)___")
+    input1 = input("What is name? ")
+    input2 = input("What is subject? ")
+    print(input1 + "'s favorite subject in school is " + input2 + ".")
+
+
+madLib()
