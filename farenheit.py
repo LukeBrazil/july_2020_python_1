3340818d2ddaeb19e62460ab9ee66fa5cc894fac
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-29 13:49:58 -0500

    Changes

--- farenheit.py
+++ farenheit.py
@@ -2,6 +2,6 @@ Celsius = int(input("Enter temperature in Celcuis: "))
 
 def tempConverter(Celsius):
     Fahrenheit = Celsius * 1.8 + 32
-    print("The conversion from Celsius to Fahrenheit = " + str(Fahrenheit))
+    print("The conversion from Celsius to Fahrenheit = " + str(float(Fahrenheit)))
 
 tempConverter(Celsius)
