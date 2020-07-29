e28bd880661a1ff4320dc2231007bb260e819db4
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-29 10:46:00 -0500

    Initial commit

---
+++ sleepin.py
@@ -0,0 +1,20 @@
+def dayNumbers():
+    day = int(input("What day is it? "))
+    if day == 0:
+        print("Sleep in")
+    elif day == 1:
+        print("Go to work")
+    elif day == 2:
+        print("Go to work")
+    elif day == 3:
+        print("Go to work")
+    elif day == 4:
+        print("Go to work")
+    elif day == 5:
+        print("Go to work")
+    elif day == 6:
+        print("Sleep in")
+    else:
+        print("Not a day!")
+    
+dayNumbers()
