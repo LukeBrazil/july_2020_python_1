c39a820b22602ea594e15a1840844374bc444464
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-29 11:47:20 -0500

    Modified while loop to print letters from the title instead of counting upfrom 0 to 10

--- loops.py
+++ loops.py
@@ -2,7 +2,7 @@ title = "I like cheese"
 STOP = 10
 counter = 0
 
-while counter < STOP:
-    print(counter)
+while counter < len(title):
+    if (counter % 2) == 0:
+        print(title[counter])
     counter += 1
-    print(counter)
