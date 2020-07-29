14d8febaba91ffebd4de6ac34cb494d41f0e9db0
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-29 13:25:28 -0500

    Initial commit

--- loops.py
+++ loops.py
@@ -1,8 +1,19 @@
-title = "I like cheese"
-STOP = 10
+# title = "I like cheese"
+# STOP = 10
+# counter = 0
+
+# while counter < len(title):
+#     if (counter % 1) == 0:
+#         print(title[counter])
+#     counter += 1
+
+
 counter = 0
 
-while counter < len(title):
-    if (counter % 1) == 0:
-        print(title[counter])
-    counter += 1
+def count():
+    counter = 0
+    while counter < 10:
+        counter += 1
+        print(counter)
+
+count()
