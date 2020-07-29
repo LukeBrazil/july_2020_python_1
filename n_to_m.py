a1b53f4d4143f7556490542db5969496a906ae99
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-29 14:05:36 -0500

    Cleaned up code

--- n_to_m.py
+++ n_to_m.py
@@ -1,9 +1,24 @@
 
 
-def count():
-    counter = 1
-    while counter < 8:
-        counter += 1
-        print(counter)
+# def count():
+#     counter = 1
+#     while counter < 8:
+#         counter += 1
+#         print(counter)
 
-count()
+# count()
+
+# def counter():
+#     output = input1
+#     while input2 > input1:
+#         output += 1
+#     print(output)
+
+# counter()
+
+input1 = int(input("Please enter starting number: "))
+input2 = int(input("Please enter an ending number: "))
+
+while input1 <= input2:
+    print(input1)
+    input1 += 1
