a412fc60cf4559e2ec09e9d81b26913053324fc1
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-29 15:44:22 -0500

    Initial commit

---
+++ lists.py
@@ -0,0 +1,74 @@
+# pets = ["cat", "salmon", "velociraptor", "dog"]
+
+# # count = 0
+
+# # while count < len(pets):
+# #     print(pets[count])
+# #     count += 1
+# # for every [needle] in a [haystack]
+# # for pet in pets:
+# #     print(pet)
+# #     if pet == "salmon":
+# #         break
+
+# if "dog" in pets:
+#     pets.remove("dog")
+#     print(pets)
+# else:
+#     print("I want doggo")
+
+
+# 1
+# list = [1, 2, 3, 4, 5]
+# print(sum(list))
+
+# 2 
+# list = [1, 2, 3, 4, 5]
+# print(max(list))
+
+# 3
+# list = [1, 2, 3, 4, 5]
+# print(min(list))
+
+#4
+# list = [1, 2, 3, 4, 5]
+
+# for list in list:
+#     if list %2 == 0:
+#         print(list)
+
+#5
+# list = [0, 1, 2, 3, 4, 5]
+
+# for list in list:
+#     if list > 0:
+#         print(list)
+
+#6
+# list1 = [-1, 1, 2, 3, 4, 5, 6]
+# list2 = []
+
+# for list1 in list1:
+#     if list1 > 0:
+#         list2.append(list1)
+
+# print(list2)
+
+# # 7
+# numbers = [1, 2, 3, 4, 5]
+# factor = 7
+# list2 = []
+
+# for number in numbers:
+#     new_number = number * factor
+#     list2.append(new_number)
+
+# print(new_number)
+
+#8
+# string = "I like dogs"[::-1]
+# print(string)
+
+words = ["hello", "goodbye", "poop"]
+words.reverse()
+print(words)
