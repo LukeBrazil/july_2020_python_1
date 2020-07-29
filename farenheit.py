a755029eb272652fcbf9abebb1ad2d2e6c7b4368
Luke Brazil <Luke.brazil13@icloud.com>
2020-07-29 11:09:45 -0500

    Fixed spelling errors

--- farenheit.py
+++ farenheit.py
@@ -1,7 +1,7 @@
-Cel = int(input("Enter temperature in Celcuis: "))
+Celsius = int(input("Enter temperature in Celcuis: "))
 
 def tempConverter(Cel):
-    Farenheit = Cel * 1.8 + 32
-    print("The conversion from Celcius to Farenheit = " + str(Farenheit))
+    Fahrenheit = Cel * 1.8 + 32
+    print("The conversion from Celsius to Fahrenheit = " + str(Fahrenheit))
 
-tempConverter(Cel)
+tempConverter(Celsius)
